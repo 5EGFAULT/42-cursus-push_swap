2771fe4a56707fabb332d99ea08d3831e21f6569
Souinia Ayoub <souiniaayoub@gmail.com>
2022-03-09 21:27:23 +0100

    push22 is working under 100

---
+++ 500 case not ok but almost/push_swap22/src/quick_sort_arr.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quick_sort_arr.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asouinia <asouinia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 10:41:17 by asouinia          #+#    #+#             */
+/*   Updated: 2022/03/08 10:57:12 by asouinia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+static void	swap(int *a, int*b)
+{
+	int	t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+}
+
+static int	partition(int *arr, int low, int high)
+{
+	int	pivot;
+	int	i;
+	int	j;
+
+	pivot = arr[high];
+	i = (low - 1);
+	j = low;
+	while (j <= high - 1)
+	{
+		if (arr[j] <= pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+		}
+		j++;
+	}
+	swap(&arr[i + 1], &arr[high]);
+	return (i + 1);
+}
+
+static void	quicksort(int *arr, int low, int high)
+{
+	int	pi;
+
+	if (low < high)
+	{
+		pi = partition(arr, low, high);
+		quicksort(arr, low, pi - 1);
+		quicksort(arr, pi + 1, high);
+	}
+}
+
+int	*sort_stack_k(t_d_list *s)
+{
+	int	*k;
+	int	i;
+
+	k = malloc(sizeof(int) * ft_d_lstsize(s));
+	if (!k)
+		exit(1);
+	i = 0;
+	while (s)
+	{
+		k[i++] = s->content;
+		s = s->next;
+	}
+	quicksort(k, 0, i - 1);
+	return (k);
+}
